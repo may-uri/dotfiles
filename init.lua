e83759435c8c416cd854cd7493ec9f8082cde480
may-uri <ndksajndsak1@gmail.com>
2023-08-30 10:06:24 +0300

    init astronvim lua

---
+++ init.lua
@@ -0,0 +1,51 @@
+if vim.loader and vim.fn.has "nvim-0.9.1" == 1 then vim.loader.enable() end
+
+for _, source in ipairs {
+  "astronvim.bootstrap",
+  "astronvim.options",
+  "astronvim.lazy",
+  "astronvim.autocmds",
+  "astronvim.mappings",
+} do
+  local status_ok, fault = pcall(require, source)
+  if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
+end
+
+if astronvim.default_colorscheme then
+  if not pcall(vim.cmd.colorscheme, astronvim.default_colorscheme) then
+    require("astronvim.utils").notify(
+      ("Error setting up colorscheme: `%s`"):format(astronvim.default_colorscheme),
+      vim.log.levels.ERROR
+    )
+  end
+end
+
+require("astronvim.utils").conditional_func(astronvim.user_opts("polish", nil, false), true)
+
+-- my config
+-- Save with Ctrl+S
+vim.api.nvim_set_keymap("n", "<C-S>", ":w<CR>", { noremap = true })
+-- Close without saving with Ctrl+Q
+vim.api.nvim_set_keymap("n", "<C-Q>", ":q!<CR>", { noremap = true })
+-- Copy with Ctrl+C
+vim.api.nvim_set_keymap("v", "<C-c>", '"+y', { noremap = true })
+-- Map F1 key to Telescope find by file
+vim.api.nvim_set_keymap("n", "<F1>", ":Telescope find_files<CR>", { noremap = true })
+
+-- Map F2 key to Telescope find by grep
+vim.api.nvim_set_keymap("n", "<F2>", ":Telescope live_grep<CR>", { noremap = true })
+
+-- Disable Ctrl+Z
+vim.api.nvim_set_keymap("n", "<C-z>", "u", { noremap = true })
+
+-- Run current file in Node.js
+vim.api.nvim_set_keymap("n", "<A-e>", ":!node %<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>e", ":!node %<CR>", { noremap = true })
+-- Go to end of line with "1"
+vim.api.nvim_set_keymap("n", "1", "$", { noremap = true })
+-- Disable netrw banner
+vim.g.netrw_banner = 0
+-- Open init.lua 
+vim.api.nvim_set_keymap('n', 'NN', ':tabnew $MYVIMRC<CR>', { noremap = true })
+-- ctrl+a to select all
+vim.api.nvim_set_keymap('n', '<C-a>', 'ggVG', { noremap = true, silent = true })

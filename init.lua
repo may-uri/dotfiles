99f12601815a66cdfa169bac99db29e4cbf0ea03
may-uri <r89647@gmail.com>
2023-09-21 18:12:36 +0300

    refactor

--- init.lua
+++ init.lua
@@ -67,4 +67,6 @@ vim.keymap.set('i', '<ScrollWheelUp>', '<C-y>')
 vim.keymap.set('i', '<ScrollWheelDown>', '<C-e>')
 vim.keymap.set('v', '<ScrollWheelUp>', '<C-y>')
 vim.keymap.set('v', '<ScrollWheelDown>', '<C-e>')
-vim.cmd('TSEnable highlight')
+-- if vim.bo.filetype ~= 'html' and vim.bo.filetype ~= 'css' and vim.bo.filetype ~= 'js' then
+
+-- end

857c03e34da9b8b929aa27e2fac618411f6919c4
may-uri <r89647@gmail.com>
2023-09-26 23:28:29 +0300

    refactor

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,14 +1,42 @@
 local wezterm = require 'wezterm'
 local act = wezterm.action
 local config = {}
-local mux = wezterm.mux
+-- local mux = wezterm.mux
 config.window_decorations = "RESIZE"
 config.adjust_window_size_when_changing_font_size = false
 config.font = wezterm.font { family = 'IosevkaTerm Nerd Font Mono' }
 config.audible_bell = 'Disabled'
-config.color_scheme = 'Everforest Dark (Gogh)'
+
+-- COLORS_SCHEME
+
+-- config.color_scheme = 'Everforest Dark (Gogh)'
+-- config.color_scheme = 'hardhacker'
+-- config.color_scheme = 'Kasugano (terminal.sexy)'
+-- config.color_scheme = 'Gruvbox dark, pale (base16)'
+-- config.color_scheme = 'Gruvbox dark, soft (base16)'
+-- config.color_scheme = 'Gruvbox Material (Gogh)'
+-- config.color_scheme = 'GitHub Dark'
+-- config.color_scheme = 'Google (dark) (terminal.sexy)'
+-- config.color_scheme = 'Kanagawa (Gogh)'
+-- config.color_scheme = 'Ashes (dark) (terminal.sexy)'
+-- config.color_scheme = 'Mariana'
+-- config.color_scheme = 'Mocha (base16)'
+-- config.color_scheme = 'Zenburn (Gogh)'
+config.color_scheme = 'Black Metal (Immortal) (base16)'
+-- config.color_scheme = 'Canvased Pastel (terminal.sexy)'
+-- config.color_scheme = 'Catch Me If You Can (terminal.sexy)'
+-- config.color_scheme = 'DanQing (base16)'
+-- config.color_scheme = 'Dark Violet (base16)'
+-- config.color_scheme = 'Dawn (terminal.sexy)'
+-- config.color_scheme = 'Embers (dark) (terminal.sexy)'
+-- config.color_scheme = 'Fahrenheit'
+-- config.color_scheme = 'Fideloper'
+-- config.color_scheme = 'Geohot (Gogh)'
+-- config.color_scheme = 'Grayscale (dark) (terminal.sexy)'
+
+
 config.exit_behavior = 'Close'
-config.font_size = 20
+config.font_size = 18
 config.initial_rows = 15
 config.initial_cols = 80
 config.hide_mouse_cursor_when_typing = true
@@ -25,13 +53,13 @@ config.keys = {
   { action = wezterm.action.ToggleFullScreen, key = 'F11' },
 }
   -- ALT + number to activate that tab
-for i = 1, 8 do
-  table.insert(config.keys, {
-    key = tostring(i),
-    mods = 'ALT',
-    action = act.ActivateTab(i - 1),
-  })
-end
+-- for i = 1, 8 do
+--   table.insert(config.keys, {
+--     key = tostring(i),
+--     mods = 'ALT',
+--     action = act.ActivateTab(i - 1),
+--   })
+-- end
 
 config.scrollback_lines = 10000
 config.show_update_window = true

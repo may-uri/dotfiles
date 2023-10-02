6fc4c71b1ef25f05a91c13fa9da514f531a98cf7
seraph <r89647@gmail.com>
2023-10-02 20:51:19 +0300

    feat: statusline ui changes, vscode_vim config

--- nvchad/lua/core/mappings.lua
+++ nvchad/lua/core/mappings.lua
@@ -83,7 +83,8 @@ M.general = {
     ["<Down>"] = { 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', "Move down", opts = { expr = true } },
 
     -- new buffer
-    ["<leader>b"] = { "<cmd> enew <CR>", "New buffer" },
+    -- ["<leader>b"] = { "<cmd> enew <CR>", "New buffer" },
+    ["<leader>b"] = { "<cmd> Telescope buffers <CR>", "telescope buffers" },
     ["<leader>ch"] = { "<cmd> NvCheatsheet <CR>", "Mapping cheatsheet" },
 
     -- ["<leader>fm"] = {
--- nvchad/lua/custom/chadrc.lua
+++ nvchad/lua/custom/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "vscode_dark",
-  theme_toggle = { "vscode_dark", "one_light" },
+  theme = "gruvchad",
+  theme_toggle = { "gruvchad", "one_light" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,

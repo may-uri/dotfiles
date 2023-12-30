3222d5325625e90573d4d9186cace85244ad10d4
wingedseraph <r89647@gmail.com>
2023-12-31 02:47:08 +0300

    feat: new keymaps

--- nvchad/lua/custom/mappings.lua
+++ nvchad/lua/custom/mappings.lua
@@ -1,5 +1,9 @@
 ---@type MappingsTable
 local M = {}
+-- Use wslview for gx in WSL
+if vim.fn.has("win32") and vim.fn.has("unix") then
+	vim.g.netrw_browsex_viewer = "wslview"
+end
 
 function Zen()
 	vim.cmd("set laststatus=2")
@@ -13,23 +17,15 @@ end
 function DisableStatusLine()
 	vim.o.laststatus = 0
 end
--- Function to toggle status line and warn before closing buffer without saving
-function ToggleAndWarn()
-	-- Check for unsaved changes
-	local unsaved_changes = vim.fn.getbufvar("%", "&modified") == 1
-	if unsaved_changes then
-		local choice = vim.fn.confirm("Buffer has unsaved changes. Do you really want to quit?", "&Yes\n&No", 2)
-		if choice == 2 then
-			return
-		end
-	end
-	-- Quit or close the buffer
-	vim.cmd("quit!")
-end
 -- vim.lsp.buf.format({ async = true })
 M.general = {
 	n = {
 
+		["<leader>gx"] = {
+			"<cmd>VisitLinkNearest<CR>",
+			"go to link gx",
+			opts = { nowait = true },
+		},
 		["<leader>gf"] = {
 			"vi{gf}",
 			"go to link gf",
@@ -64,7 +60,7 @@ M.general = {
 		},
 		["<leader>o"] = { ":Telescope oldfiles theme=get_ivy <CR>", "[T]elescope [O]ldfiles", opts = { nowait = true } },
 		["<leader>e"] = { ":!node %<CR>", "[R]un [N]ode", opts = { nowait = true } },
-		-- ["<leader>rr"] = { "<cmd>SnipClose<CR>", "[S]nip [C]lose", opts = { nowait = true } },
+		["<leader>rr"] = { "<cmd>SnipClose<CR>", "[S]nip [C]lose", opts = { nowait = true } },
 		["<leader>x"] = { "<cmd>bdelete %<CR>", "close buffer", opts = { nowait = true } },
 		["<leader>lg"] = { "<cmd>LazyGit<CR>", "lazygit", opts = { nowait = true } },
 		["<leader>ww"] = { "<cmd>set wrap!<CR>", "toggle wrap", opts = { nowait = true } },
@@ -78,20 +74,18 @@ M.general = {
 		-- ["<C-s>"] = { ":w<CR>", "[S]ave [F]ile", opts = { nowait = true } },
 		-- ["<M-b>"] = { "<cmd>Oil<CR>", "[O]il", opts = { nowait = true } },
 		["<C-q>"] = { "<cmd>q!<CR>", "quit without save", opts = { nowait = true } },
-		-- ["<C-q>"] = { "<cmd>lua ToggleAndWarn()<CR>", "[Q]uit [W]ithout [S]ave", opts = { nowait = true } },
 		["<C-z>"] = { "u", "[U]ndo", opts = { nowait = true } },
 		["<C-a>"] = { "gg<S-v>G", "[S]elect [A]ll", opts = { nowait = true } },
-
 		["<F1>"] = { "<cmd> Telescope find_files <CR>", "Live grep" },
 		["<F2>"] = { "<cmd> Telescope live_grep <CR>", "Live grep" },
 		["NN"] = { ":tabnew $MYVIMRC<CR>", "Open init.lua", opts = { nowait = true } },
-		-- [";"] = { ":", "enter command mode", opts = { nowait = true } },
+		[";"] = { ":", "enter command mode", opts = { nowait = true } },
 		["1"] = { "$", "go to end of line", opts = { nowait = true } },
 	},
 	v = {
 		["<C-c>"] = { '"+y', "[C]opy", opts = { nowait = true } },
 
-		-- ["<leader>rr"] = { ":SnipRun<CR>", "[S]nip [R]un", opts = { nowait = true } },
+		["<leader>rr"] = { ":SnipRun<CR>", "[S]nip [R]un", opts = { nowait = true } },
 		[">"] = { ">gv", "indent" },
 		["ge"] = { "G", "[L]ast [Line]", opts = { nowait = true } },
 		["1"] = { "$h", "go to end of line", opts = { nowait = true } },
